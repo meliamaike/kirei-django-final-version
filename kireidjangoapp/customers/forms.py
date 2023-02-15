98c5f8d8e0c712c2a37919cb6a4b2e4e37791bb3
meli <junname@MacBook-Pro-de-meli.local>
2023-02-15 18:23:45 -0300

    Adding styles to login and signup

--- kireidjangoapp/customers/forms.py
+++ kireidjangoapp/customers/forms.py
@@ -25,7 +25,6 @@ class RegisterForm(SignupForm):
     document_number = forms.CharField(
         label="Nro. Documento", max_length=30, required=True
     )
-    #email = forms.EmailField(max_length=255, required=True, label="Email")
     area_code = forms.CharField(label="Código Área", max_length=5, required=True)
     phone_number = forms.CharField(label="Nro. Teléfono", max_length=20, required=True)
     
@@ -41,35 +40,32 @@ class RegisterForm(SignupForm):
 
     class Meta:
         model = get_user_model()
+    
+    
 
 
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.helper = FormHelper(self)
-    #     self.fields["email"].label = "Email"
-    #     self.fields["password1"].label = "Contraseña"
-    #     self.fields["password2"].label = "Confirmá tu contraseña"
-    #     self.fields["email"].widget.attrs["placeholder"] = ""
-    #     self.fields["password1"].widget.attrs["placeholder"] = ""
-    #     self.fields["password2"].widget.attrs["placeholder"] = ""
-    #     self.helper.layout = Layout(
-    #         Row(
-    #             Column("first_name", css_class="form-group col-md-6"),
-    #             Column("last_name", css_class="form-group col-md-6"),
-    #             css_class="form-row",
-    #         ),
-    #         "email",
-    #         Row(
-    #             Column("document_number", css_class="form-group col-md-6"),
-    #             Column("phone_number", css_class="form-group col-md-6"),
-    #             css_class="form-row",
-    #         ),
-    #         Row(
-    #             Column("password1", css_class="form-group col-md-6"),
-    #             Column("password2", css_class="form-group col-md-6"),
-    #         ),
-    #         Submit("submit", "Ingresar", css_class="btn btn-primary mt-3"),
-    #     )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.fields["email"].label = "Email"
+        self.fields["password1"].label = "Contraseña"
+        self.fields["password2"].label = "Confirmá tu contraseña"
+        self.fields["email"].widget.attrs["placeholder"] = ""
+        self.fields["password1"].widget.attrs["placeholder"] = ""
+        self.fields["password2"].widget.attrs["placeholder"] = ""
+        self.fields['first_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['last_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['document_number'].widget.attrs.update({'class': 'form-control'})
+        self.fields['area_code'].widget.attrs.update({'class': 'form-control'})
+        self.fields['phone_number'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+
+
+
+
+    
 
 # Form de Login
 
@@ -83,11 +79,7 @@ class CustomerLoginForm(LoginForm):
         self.fields["password"].label = "Contraseña"
         self.fields["remember"].label = "Recuérdame"
         self.helper.form_method = "post"
-        # self.helper.layout = Layout(
-        #     "login",
-        #     "password",
-        #     "remember",
-        # )
-
         self.fields["password"].widget.attrs["placeholder"] = ""
         self.fields["login"].widget.attrs["placeholder"] = ""
+        self.fields['login'].widget.attrs.update({'class': 'form-control my-2'})
+        self.fields['password'].widget.attrs.update({'class': 'form-control my-2'})
